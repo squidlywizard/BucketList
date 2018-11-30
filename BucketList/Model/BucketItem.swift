674601178f63e646ebd15d4c9de5ed379f18adbf
squidlywizard <ztdenna@hotmail.com>
2018-11-30 07:53:49 -0700

    files were added
    a cvs file that has animals and dreams,
    a model,
    and 2 control

---
+++ BucketList/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  BucketList
+//
+//  Created by Denna, Zachary on 11/30/18.
+//  Copyright © 2018 Denna, Zachary. All rights reserved.
+//
+
+import Foundation
---
+++ BucketList/control/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  BucketList
+//
+//  Created by Denna, Zachary on 11/30/18.
+//  Copyright © 2018 Denna, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
